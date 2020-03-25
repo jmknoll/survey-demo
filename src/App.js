ad6fbc137067c727881da0b5a3ed692f752b3135
Jameson Knoll <j.knoll621@gmail.com>
2020-03-24 20:06:47 -0400

    remove initial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  alert("bonesaw is also ready");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>test</p>
     </div>
   );
 }
 
 export default App;
+
+/*
+TODO
+1. Proof out browser interactions - opening the popup && passing logic back and forth
+2. Proof out local storage sync. Callback on form actions?
+3. Handle application state with context/reducer hook
+3. Handle form state - reusable input with logic?
+*/
