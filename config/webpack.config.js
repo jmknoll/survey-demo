aebf3562bd03cf4fd0e25f18fe9921829a320e2d
Jameson Knoll <j.knoll621@gmail.com>
2020-04-03 11:25:54 -0400

    update config for cdn deploy

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -159,39 +159,12 @@ module.exports = function(webpackEnv) {
       // changing JS code would still trigger a refresh.
     ].filter(Boolean),
     output: {
-      // The build folder.
-      path: isEnvProduction ? paths.appBuild : undefined,
-      // Add /* filename */ comments to generated require()s in the output.
-      pathinfo: isEnvDevelopment,
-      // There will be one main bundle, and one file per asynchronous chunk.
-      // In development, it does not produce real files.
-      filename: isEnvProduction
-        ? "static/js/[name].[contenthash:8].js"
-        : isEnvDevelopment && "static/js/bundle.js",
-      // TODO: remove this when upgrading to webpack 5
-      futureEmitAssets: true,
-      // There are also additional JS chunk files if you use code splitting.
-      chunkFilename: isEnvProduction
-        ? "static/js/[name].chunk.js"
-        : isEnvDevelopment && "static/js/[name].chunk.js",
-      // webpack uses `publicPath` to determine where the app is being served from.
-      // It requires a trailing slash, or the file assets will get an incorrect path.
-      // We inferred the "public path" (such as / or /my-project) from homepage.
-      publicPath: paths.publicUrlOrPath,
-      // Point sourcemap entries to original disk location (format as URL on Windows)
-      devtoolModuleFilenameTemplate: isEnvProduction
-        ? info =>
-            path
-              .relative(paths.appSrc, info.absoluteResourcePath)
-              .replace(/\\/g, "/")
-        : isEnvDevelopment &&
-          (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, "/")),
-      // Prevents conflicts when multiple webpack runtimes (from different apps)
-      // are used on the same page.
-      jsonpFunction: `webpackJsonp${appPackageJson.name}`,
-      // this defaults to 'window', but by setting it to 'this' then
-      // module chunks which are built will work in web workers as well.
-      globalObject: "this"
+      path: paths.appBuild,
+      filename: "./js/main.js",
+      globalObject: "this",
+      library: "Survey",
+      libraryTarget: "umd",
+      umdNamedDefine: true
     },
     optimization: {
       minimize: isEnvProduction,
@@ -256,20 +229,20 @@ module.exports = function(webpackEnv) {
             preset: ["default", { minifyFontValues: { removeQuotes: false } }]
           }
         })
-      ],
-      // Automatically split vendor and commons
-      // https://twitter.com/wSokra/status/969633336732905474
-      // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-      splitChunks: {
-        chunks: "all",
-        name: false
-      },
-      // Keep the runtime chunk separated to enable long term caching
-      // https://twitter.com/wSokra/status/969679223278505985
-      // https://github.com/facebook/create-react-app/issues/5358
-      runtimeChunk: {
-        name: entrypoint => `runtime-${entrypoint.name}`
-      }
+      ]
+      // // Automatically split vendor and commons
+      // // https://twitter.com/wSokra/status/969633336732905474
+      // // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+      // splitChunks: {
+      //   chunks: "all",
+      //   name: false
+      // },
+      // // Keep the runtime chunk separated to enable long term caching
+      // // https://twitter.com/wSokra/status/969679223278505985
+      // // https://github.com/facebook/create-react-app/issues/5358
+      // runtimeChunk: {
+      //   name: entrypoint => `runtime-${entrypoint.name}`
+      // }
     },
     resolve: {
       // This allows you to set a fallback for where webpack should look for modules.
